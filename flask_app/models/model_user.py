56ad0c2a935bb81561bc4ec9975b4dc0b495c151
GBann <GTBannon@protonmail.com>
2023-07-25 15:18:53 -0600

    added user model functions

--- flask_app/models/model_user.py
+++ flask_app/models/model_user.py
@@ -1,5 +1,12 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_app import app
 
 DATABASE = "weight_lifter_db"
+
+#REGEX
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 
 class User:
@@ -18,4 +25,66 @@ class User:
         self.acc_setup = data['acc_setup']
         
 
+    @classmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['first_name']) < 3:
+            flash("*First name must be at least 3 characters")
+            is_valid = False
+
+        if len(user['last_name']) < 3:
+            flash("*Last name must be at least 3 characters")
+            is_valid = False
+
+        print(user['email'])
+        if len(user['email']) < 5:
+            flash("*Email must be at least 5 characters")
+            is_valid = False
+
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("*Invalid email address")
+            is_valid = False
+
+        if len(user['password']) < 8:
+            flash("*Password must be at least 8 characters")
+            is_valid = False
+
+        if not user['password'] == user['confirm_password']:
+            flash("*Passwords must match")
+            is_valid = False
+
+        def email_unique(email):
+            is_valid = True
+            query = "SELECT * FROM users WHERE email = %(email)s;"
+            results = connectToMySQL(DATABASE).query_db(query, {'email':email})
+            if len(results) > 0:
+                is_valid = False
+            return is_valid
+
+        if not email_unique(user['email']):
+            flash("*This email is already in use")
+            is_valid = False
+        return is_valid
     
+    #Used for adding a user to the database
+    @classmethod
+    def write_user(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
+        id = connectToMySQL(DATABASE).query_db(query, data)
+        return id
+    
+    @classmethod
+    def get_one(cls, id):
+        query = "SELECT * FROM users WHERE users.id = %(id)s;"
+        results = connectToMySQL(DATABASE).query_db(query, {'id':id})
+        user = cls(results[0])
+        return user
+
+    #===== LOGIN METHOD =====
+    @classmethod
+    def login(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL(DATABASE).query_db(query, data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
